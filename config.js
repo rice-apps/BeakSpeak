0ebc6aad1e92a31728b252d3781241c10649eaa5
nquazi <noushinquazi@yahoo.com>
2018-08-23 17:14:54 -0500

    fixing config management

---
+++ config.js
@@ -0,0 +1,22 @@
+import { environment } from './Environments/environment';
+
+export let CONFIG;
+
+rice_ip = "10.112.176.253"
+curr_ip = "10.0.0.193"
+ip = rice_ip
+if (environment.production) {
+    CONFIG = {
+        api_url: 'https://speak.riceapps.org/api',
+        cas_auth_url: 'https://idp.rice.edu/idp/profile/cas/login',
+        cas_logout_url: 'https://idp.rice.edu/idp/profile/cas/logout',
+        service_url: 'https://speak.riceapps.org/auth'
+    };
+} else {
+    CONFIG = {
+        api_url: 'http://'+ip+':3000/api',
+        cas_auth_url: 'https://idp.rice.edu/idp/profile/cas/login',
+        cas_logout_url: 'https://idp.rice.edu/idp/profile/cas/logout',
+        service_url: 'http://localhost:4200/auth'
+    };
+}
