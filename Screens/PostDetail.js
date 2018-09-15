156b732e2f2aa5a87f24d4d6db0c4f0c58347f98
Alice Wong <ajw7@rice.edu>
2018-09-15 16:02:19 -0500

    get data for posts and copy changes from master

--- Screens/PostDetail.js
+++ Screens/PostDetail.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 
-import DatabaseService from '../Services/DatabaseService'
+import DatabaseService, {getPost} from '../Services/DatabaseService'
 import Blank from '../Components/Blank'
 
 //TODO: implement this class
@@ -22,9 +22,17 @@ export default class PostDetailScreen extends Component{
         this.mounted = true
 
         post_id = this.props.navigation.getParam('id') // use this post id to query the individual post from the backend
-        let post = null // put database logic here -- look in Servcies/DatabaseService for the appropriate method
+        // use await to make sure there's a value
+        let post = await DatabaseService.getPost(post_id) // put database logic here -- look in Servcies/DatabaseService for the appropriate method
+        console.log(post)
+
         if(this.mounted) { // set state here to avoid memory leak
-            console.log('make sure to set refresh to false and load to true -- like in Main screen!')
+            this.setState({
+                post: post,
+                loaded: true,
+                refresh: false
+            })
+            //console.log('make sure to set refresh to false and load to true -- like in Main screen!')
         }
     }
 
@@ -35,11 +43,35 @@ export default class PostDetailScreen extends Component{
 
     // render a post with comments -- use posts component from main as an example for structure
     render = () => {
-        return(
-            <Blank/> //placeholder, delete with actual code 
-            /*Use Post from Components folder*/
-            /*Use Comment from Componenst folder and construct comments*/
-            /*BONUS: create an input field for new comments*/
-        )
+        let loaded = this.state.loaded
+        if(!loaded) { // wait for posts to load
+            return(
+                <AppLoading/>
+            )
+        }
+
+        else {
+            let refresh = this.state.refresh
+            let comments = this.state.comments
+
+            return (
+                <View>
+                <Post
+                    title = {post.title}
+                    body = {post.body}/>
+                {/*//<Comment />*/}
+
+                </View>
+            )
+
+        }
+
+
+        // return(
+        //     <Blank/> //placeholder, delete with actual code
+        //     /*Use Post from Components folder*/
+        //     /*Use Comment from Components folder and construct comments*/
+        //     /*BONUS: create an input field for new comments*/
+        // )
     }
 }
