6265f6381252d6346f0af2cfdf0c213234dd54eb
Alice Wong <ajw7@rice.edu>
2018-10-20 15:29:13 -0500

    Delete multiline

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ import { environment } from './Environments/environment';
 
 export let CONFIG;
 
-ip = "10.123.178.152"
+ip = "10.123.178.249"
 if (environment.production) {
     CONFIG = {
         api_url: 'https://speak.riceapps.org/api',
