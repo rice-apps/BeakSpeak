689e489b63d1715faabcdbe8591268dc8efe9455
TheraFu <therafuminqi@gmail.com>
2018-11-10 14:26:57 -0600

    Opens idp

--- Screens/Front.js
+++ Screens/Front.js
@@ -91,36 +91,21 @@ export default class FrontScreen extends Component {
         let isVisible = this.state.modalVisible;
         
         return (
-            <WebviewLogin/>
+            <View style={[styles.screenTheme, {height: screenHeight}]}>
 
-            // <View style={[styles.screenTheme, {height: screenHeight}]}>
-                    
-                //     {/* login modal */}
-                //     <Modal
-                //         isVisible = {isVisible}
-                //         animationIn = {'slideInUp'}
-                //         animationOut = {'zoomOut'}
-                //         animationInTiming = {500}
-                //         animationOutTiming = {500}
-                //         avoidKeyboard
-                //     >
-                //         <View style={{
-                //                         borderRadius: 10,
-                //                         padding: 10,
-                //                         backgroundColor: 'white'
-                //                     }}>
-                //             <View style={{
-                //                             flexDirection: 'row',
-                //                             justifyContent: 'flex-end'
-                //                         }}>
-                //             </View>
-                //
-                //             {/* new post creation form*/}
-                //             {/*<Login closeView = {this.hideModal}/>*/}
-                //         </View>
-                //     </Modal>
-                //     <FrontBody/>
-                // </View>
+                    {/* login modal */}
+                    <Modal
+                        isVisible = {isVisible}
+                        animationIn = {'slideInUp'}
+                        animationOut = {'zoomOut'}
+                        animationInTiming = {500}
+                        animationOutTiming = {500}
+                        avoidKeyboard
+                    >
+                        <WebviewLogin closeView = {this.hideModal}/>
+                    </Modal>
+                    <FrontBody/>
+                </View>
         );
     }
 }
--- Screens/WebviewLogin.js
+++ Screens/WebviewLogin.js
@@ -2,9 +2,25 @@ import React, { Component } from 'react';
 import { WebView } from 'react-native';
 
 class Webviewlogin extends Component {
+
+    constructor(props){
+        super(props);
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(props){
+        if(!props.loading){
+            if(props.url.includes('https://speak.riceapps.org/auth?ticket=')){
+                console.log(props.url.substring(props.url.indexOf("ticket="), props.url.length))
+                this.props.closeView();
+            }
+        }
+    }
+
     render() {
         return (
             <WebView
+                onNavigationStateChange={this.handleChange}
                 source={{uri: 'https://idp.rice.edu/idp/profile/cas/login?service=https://speak.riceapps.org/auth'}}
                 style={{marginTop: 20}}
             />
