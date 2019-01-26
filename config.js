373343cdc587c427cc33d98a999d0edb11bfa6a8
parkergraham <parkermgraham@gmail.com>
2019-01-26 13:12:12 -0600

    test

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ import { environment } from './Environments/environment';
 
 export let CONFIG;
 
-ip = "YOUR IP ADDRESS HERE"
+ip = "10.122.177.56"
 if (environment.production) {
     CONFIG = {
         api_url: 'https://speak.riceapps.org/api',
