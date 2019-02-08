30dde45b122969b27673181f081c3f318d61dc7e
nquazi <noushinquazi@yahoo.com>
2019-02-08 16:58:45 -0600

    removed ip address from config

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ import { environment } from './Environments/environment';
 
 export let CONFIG;
 
-ip = "10.112.177.238"
+ip = "YOUR IP ADDRESS HERE"
 if (environment.production) {
     CONFIG = {
         api_url: 'https://speak.riceapps.org/api',
