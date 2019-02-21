c7b84928cf6ab6b77be537a9043e8a53331e7b74
nquazi <noushinquazi@yahoo.com>
2019-02-20 19:09:54 -0600

    revert config

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ import { environment } from './Environments/environment';
 
 export let CONFIG;
 
-ip = "10.208.177.3"
+ip = "YOUR IP ADDRESS"
 if (environment.production) {
     CONFIG = {
         api_url: 'https://speak.riceapps.org/api',
