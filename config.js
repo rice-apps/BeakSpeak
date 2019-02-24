b05b097c8a79551c8f76a7b2b2620d4484bc3b68
nquazi <noushinquazi@yahoo.com>
2019-02-24 09:23:11 -0600

    removed ip address

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ import { environment } from './Environments/environment';
 
 export let CONFIG;
 
-ip = "10.0.0.193"
+ip = "YOUR IP ADDRESS"
 if (environment.production) {
     CONFIG = {
         api_url: 'https://beakspeak-backend-232019.appspot.com/api',
