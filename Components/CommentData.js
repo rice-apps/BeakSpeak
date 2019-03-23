045d97bacd814b92ebc93f2693ec7057104410fe
TheraFu <therafuminqi@gmail.com>
2019-03-23 13:39:15 -0500

    Log out feature

--- Components/CommentData.js
+++ Components/CommentData.js
@@ -1,88 +1,87 @@
-import React, {Component} from 'react'
-import {Button, FlatList, StyleSheet} from "react-native"
-import {Card} from 'native-base'
-import Comment from '../Components/Comment'
+import React, { Component } from 'react';
+import { StyleSheet } from 'react-native';
 import { observer } from 'mobx-react';
+import Comment from '../Components/Comment';
 
-
-export default CommentData = observer(
-class CommentData extends Component {
-
+let CommentData;
+export default (CommentData = observer(
+  class CommentData extends Component {
     // increment vote count up by 1
-upvoteScore = () => { 
-    old_vote = this.props.comment.userVote
-    score = this.props.comment.score
-    if (old_vote == 1) {
-        new_vote = 0
-        score -= 1
-    }
-    if (old_vote == 0) {
-        new_vote = 1
-        score += 1
-    }
-    if (old_vote == -1) {
-        new_vote = 1
-        score += 2
-    }
-    this.props.comment.updateVote(new_vote, this.props.comment._id, this.props.post_id)
-    this.props.comment.score = score
+    upvoteScore = () => {
+      let old_vote = this.props.comment.userVote;
+      let score = this.props.comment.score;
+      let new_vote;
+      if (old_vote === 1) {
+        new_vote = 0;
+        score -= 1;
+      }
+      if (old_vote === 0) {
+        new_vote = 1;
+        score += 1;
+      }
+      if (old_vote === -1) {
+        new_vote = 1;
+        score += 2;
+      }
+      this.props.comment.updateVote(new_vote, this.props.comment._id, this.props.post_id);
+      this.props.comment.score = score;
+    };
 
-}
+    // increment vote count down by 1
+    downvoteScore = async () => {
+      let old_vote = this.props.comment.userVote;
+      let score = this.props.comment.score;
+      let new_vote;
 
-// increment vote count down by 1
-downvoteScore = async() => {
-    old_vote = this.props.comment.userVote
-    score = this.props.comment.score
+      if (old_vote === 1) {
+        new_vote = -1;
+        score -= 2;
+      }
+      if (old_vote === 0) {
+        new_vote = -1;
+        score -= 1;
+      }
+      if (old_vote === -1) {
+        new_vote = 0;
+        score += 1;
+      }
+      this.props.comment.updateVote(new_vote, this.props.comment._id, this.props.post_id);
+      this.props.comment.score = score;
+    };
 
-    if (old_vote == 1) {
-        new_vote = -1
-        score -= 2
+    render() {
+      let comment = this.props.comment;
+      return (
+        <Comment
+          vote={comment.vote}
+          score={comment.score}
+          body={comment.body}
+          upvoteScore={this.upvoteScore}
+          downvoteScore={this.downvoteScore}
+          showVote={this.props.showVote}
+        />
+      );
     }
-    if (old_vote == 0) {
-        new_vote = -1
-        score -= 1
-    }
-    if (old_vote == -1) {
-        new_vote = 0
-        score += 1
-    }
-    this.props.comment.updateVote(new_vote, this.props.comment._id, this.props.post_id)
-    this.props.comment.score = score
-}
-
-
-    render () {
-        let comment = this.props.comment
-        return(
-            <Comment
-                vote = {comment.vote}
-                score = {comment.score}
-                body = {comment.body}
-                upvoteScore = {this.upvoteScore}
-                downvoteScore = {this.downvoteScore}
-                showVote = {this.props.showVote}
-            />
-        )
-    }
-})
+  }
+));
 
 const styles = StyleSheet.create({
-    container: {
+  container: {
     flex: 0,
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
   },
-    button: {
-    backgroundColor: "powderblue",
+  button: {
+    backgroundColor: 'powderblue',
     width: '15%',
     height: 15,
-    borderColor: "powderblue",
+    borderColor: 'powderblue',
     borderWidth: 15,
-    borderRadius: 15
+    borderRadius: 15,
   },
-    card: {
-        borderColor: "powderblue",
-        borderWidth: 5,
-        borderRadius: 15
-    }
-})
+  card: {
+    borderColor: 'powderblue',
+    borderWidth: 5,
+    borderRadius: 15,
+  },
+});
--- Components/Post.js
+++ Components/Post.js
@@ -1,178 +1,191 @@
-import React, {Component, PureComponent} from 'react'
-import {Card, CardItem, Title, Button, Text, Icon} from 'native-base'
-import {StyleSheet, View} from 'react-native'
+import React, { Component, PureComponent } from 'react';
+import { CardItem, Button, Text, Icon } from 'native-base';
+import { StyleSheet, View } from 'react-native';
 
 // body with post content and potentially votes
-class PostBody extends PureComponent{
-
-    render() {
-        return(
-            <CardItem>
-                    <Text>
-                        {this.props.body}
-                    </Text>
-            </CardItem>
-        )
-    }
+class PostBody extends PureComponent {
+  render() {
+    return (
+      <CardItem>
+        <Text>{this.props.body}</Text>
+      </CardItem>
+    );
+  }
 }
 
 class PostVotes extends PureComponent {
-    upvoteScore = () => {
-        this.props.upvoteScore()
-    }
+  upvoteScore = () => {
+    this.props.upvoteScore();
+  };
 
-    downvoteScore = () => {
-        this.props.downvoteScore()
-    }
+  downvoteScore = () => {
+    this.props.downvoteScore();
+  };
 
-    render() {
-        let vote = this.props.vote
-        let upvoteIconColor =  vote == 1 ? "orange" : "black"
-        let downvoteIconColor = vote == -1 ?  "blue" : "black"
-        return(
-            <View style={{flex: 1, flexDirection: 'column', alignItems: 'center'}}>
-                
-                {/* upvote button */}
-                <Icon
-                    name = 'ios-arrow-up'
-                    fontSize = {30}
-                    type = 'Ionicons'
-                    style = {{color: upvoteIconColor}}
-                    onPress = {() => this.upvoteScore()}
-                />
+  render() {
+    let vote = this.props.vote;
+    let upvoteIconColor = vote === 1 ? 'orange' : 'black';
+    let downvoteIconColor = vote === -1 ? 'blue' : 'black';
+    return (
+      <View style={{ flex: 1, flexDirection: 'column', alignItems: 'center' }}>
+        {/* upvote button */}
+        <Icon
+          name="ios-arrow-up"
+          fontSize={30}
+          type="Ionicons"
+          style={{ color: upvoteIconColor }}
+          onPress={() => this.upvoteScore()}
+        />
 
-                {/* score */}                                
-                <Text>
-                    {this.props.score}
-                </Text>
-                
-                {/* downvote button */}                
-                <Icon
-                    name = 'ios-arrow-down'
-                    fontSize = {30}
-                    type = 'Ionicons'
-                    style = {{color: downvoteIconColor}}
-                    onPress = {() => this.downvoteScore()}
-                />
-            </View>
-        )
-    }
+        {/* score */}
+        <Text>{this.props.score}</Text>
+
+        {/* downvote button */}
+        <Icon
+          name="ios-arrow-down"
+          fontSize={30}
+          type="Ionicons"
+          style={{ color: downvoteIconColor }}
+          onPress={() => this.downvoteScore()}
+        />
+      </View>
+    );
+  }
 }
 
 // header with title and potentially avatar and time info
-class PostHeader extends PureComponent{
-
-    render() {
-        return(
-            <CardItem>
-                <Text style ={styles.titlefont}>
-                    {this.props.title}
-                </Text>
-            </CardItem>                
-        )
-    }
+class PostHeader extends PureComponent {
+  render() {
+    return (
+      <CardItem>
+        <Text style={styles.titlefont}>{this.props.title}</Text>
+      </CardItem>
+    );
+  }
 }
 
-class PostFooter extends PureComponent{
+class PostFooter extends PureComponent {
+  pressReact = react => {
+    this.props.updateReact(react);
+  };
 
-    pressReact = (react) => {
-        this.props.updateReact(react)
-    }
+  render() {
+    let userReact = this.props.userReact;
+    let reactCounts = this.props.reactCounts;
 
-    render() {
-        let userReact = this.props.userReact
-        let reactCounts = this.props.reactCounts
-
-        return(
-            <View style={styles.container}>
-                <Button onPress={() => this.pressReact("angry")} style={userReact == "angry" ? styles.buttonPress : {}} transparent rounded>
-                    <Text adjustsFontSizeToFit={true} style = {{color:(userReact == "angry") ? "white" : "black"}}>
-                        😡{reactCounts["angry"].toString()}</Text>
-                </Button>
-                <Button onPress={() => this.pressReact("funny")} style={userReact == "funny" ? styles.buttonPress : {}} transparent rounded>
-                    <Text adjustsFontSizeToFit={true} style = {{color:(userReact == "funny") ? "white" : "black"}}>
-                        😂{reactCounts["funny"].toString()}</Text>
-                </Button>
-                <Button onPress={() => this.pressReact("love")} style={userReact == "love" ? styles.buttonPress : {}} transparent rounded>
-                    <Text adjustsFontSizeToFit={true} style = {{color:(userReact == "love") ? "white" : "black"}}>
-                        😍{reactCounts["love"].toString()}</Text>
-                </Button>
-                <Button onPress={() => this.pressReact("sad")} style={userReact == "sad" ? styles.buttonPress : {}} transparent rounded>
-                    <Text adjustsFontSizeToFit={true} style = {{color:(userReact == "sad") ? "white" : "black"}}>
-                        😭{reactCounts["sad"].toString()}</Text>
-                </Button>
-                <Button onPress={() => this.pressReact("wow")} style={userReact == "wow" ? styles.buttonPress : {}} transparent rounded>
-                    <Text adjustsFontSizeToFit={true} style = {{color:(userReact == "wow") ? "white" : "black"}}>
-                        😮{reactCounts["wow"].toString()}</Text>
-                </Button>
-            </View>
-        )
-    }
+    return (
+      <View style={styles.container}>
+        <Button
+          onPress={() => this.pressReact('angry')}
+          style={userReact === 'angry' ? styles.buttonPress : {}}
+          transparent
+          rounded>
+          <Text adjustsFontSizeToFit style={{ color: userReact === 'angry' ? 'white' : 'black' }}>
+            😡{reactCounts['angry'].toString()}
+          </Text>
+        </Button>
+        <Button
+          onPress={() => this.pressReact('funny')}
+          style={userReact === 'funny' ? styles.buttonPress : {}}
+          transparent
+          rounded>
+          <Text adjustsFontSizeToFit style={{ color: userReact === 'funny' ? 'white' : 'black' }}>
+            😂{reactCounts['funny'].toString()}
+          </Text>
+        </Button>
+        <Button
+          onPress={() => this.pressReact('love')}
+          style={userReact === 'love' ? styles.buttonPress : {}}
+          transparent
+          rounded>
+          <Text adjustsFontSizeToFit style={{ color: userReact === 'love' ? 'white' : 'black' }}>
+            😍{reactCounts['love'].toString()}
+          </Text>
+        </Button>
+        <Button
+          onPress={() => this.pressReact('sad')}
+          style={userReact === 'sad' ? styles.buttonPress : {}}
+          transparent
+          rounded>
+          <Text adjustsFontSizeToFit style={{ color: userReact === 'sad' ? 'white' : 'black' }}>
+            😭{reactCounts['sad'].toString()}
+          </Text>
+        </Button>
+        <Button
+          onPress={() => this.pressReact('wow')}
+          style={userReact === 'wow' ? styles.buttonPress : {}}
+          transparent
+          rounded>
+          <Text adjustsFontSizeToFit style={{ color: userReact === 'wow' ? 'white' : 'black' }}>
+            😮{reactCounts['wow'].toString()}
+          </Text>
+        </Button>
+      </View>
+    );
+  }
 }
 // main component -- pure component for rendering optimization (view only)
-export default class Post extends Component{
-    render() {
-        return(
-            <View>
-                <View style={{ flex: 1, flexDirection: 'row' }}>
+export default class Post extends Component {
+  render() {
+    return (
+      <View>
+        <View style={{ flex: 1, flexDirection: 'row' }}>
+          {/* post component decomposed into children components */}
+          <View style={[{ flex: 7, justifyContent: 'center' }]}>
+            <PostHeader title={this.props.title} />
+          </View>
 
-                    {/* post component decomposed into children components */}
-                    <View style={[{ flex: 7, justifyContent: 'center' }]}>
-                        <PostHeader title={this.props.title} />
-                    </View>
+          {/* voting component */}
+          <View style={{ flex: 1 }}>
+            <PostVotes
+              vote={this.props.vote}
+              score={this.props.score}
+              upvoteScore={this.props.upvoteScore}
+              downvoteScore={this.props.downvoteScore}
+            />
+          </View>
+        </View>
 
-                    {/* voting component */}
-                    <View style = {{ flex: 1}}>
-                        <PostVotes
-                            vote = {this.props.vote}
-                            score={this.props.score}
-                            upvoteScore={this.props.upvoteScore}
-                            downvoteScore={this.props.downvoteScore}
-                        />
-                    </View>
-                </View>
-
-                {/* body of post */}
-                <PostBody body={this.props.body} />
-                <PostFooter
-                    userReact = {this.props.userReact}
-                    reactCounts = {this.props.reactCounts}
-                    updateReact = {this.props.updateReact}
-                />
-    
-            </View>
-        )
-    }
+        {/* body of post */}
+        <PostBody body={this.props.body} />
+        <PostFooter
+          userReact={this.props.userReact}
+          reactCounts={this.props.reactCounts}
+          updateReact={this.props.updateReact}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 0.5,
-        flexDirection: 'row',
-        justifyContent: 'space-evenly',
-        paddingBottom: 5
-    },
-    button: {
-        backgroundColor: "powderblue",
-        height: 35,
-        width: 70,
-        borderWidth: 0.5,
-        borderRadius: 15,
-        justifyContent: 'center'
-    },
+  container: {
+    flex: 0.5,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    paddingBottom: 5,
+  },
+  button: {
+    backgroundColor: 'powderblue',
+    height: 35,
+    width: 70,
+    borderWidth: 0.5,
+    borderRadius: 15,
+    justifyContent: 'center',
+  },
 
-    buttonPress: {
-        backgroundColor: "powderblue",
-        justifyContent: 'center',
-        flexWrap: 'wrap'
-    },
-    card: {
-        borderColor: "powderblue",
-        borderWidth: 5,
-        borderRadius: 15
-    },
-    titlefont:{
-        fontWeight: 'bold',
-        fontSize: 20
-    }
-})
+  buttonPress: {
+    backgroundColor: 'powderblue',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+  },
+  card: {
+    borderColor: 'powderblue',
+    borderWidth: 5,
+    borderRadius: 15,
+  },
+  titlefont: {
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+});
