3aba275ee94623e9c753406a81ae68ed802dd514
nquazi <noushinquazi@yahoo.com>
2019-03-23 16:06:51 -0500

    tested logout functionality

--- Components/New.js
+++ Components/New.js
@@ -1,100 +1,116 @@
-import React, { Component } from 'react';
-import { View, StyleSheet, Dimensions } from 'react-native';
-import { Button, Text } from 'native-base';
-import t from 'tcomb-form-native';
-import { inject } from 'mobx-react';
+import React, {Component} from 'react';
+import {Dimensions, StyleSheet, View} from 'react-native'
+import {Button, Text} from 'native-base'
+import t from 'tcomb-form-native'
+import {inject} from 'mobx-react'
 
 // form component
-const Form = t.form.Form;
+const Form = t.form.Form
 
 // template for new post form
 const PostSchema = t.struct({
-  title: t.String,
-  body: t.maybe(t.String),
-});
+    title: t.String,
+    body: t.maybe(t.String)
+})
 
 // get screen width
-let { width: screenWidth } = Dimensions.get('window');
+let {width: screenWidth} = Dimensions.get('window')
 
 // customizing new post form
 const PostOptions = {
-  fields: {
-    body: {
-      placeholder: 'Your thoughts here...',
-      multiline: true,
-      numberOfLines: 5,
-      blurOnSubmit: true,
-      maxLength: 1000,
-      stylesheet: {
-        ...Form.stylesheet,
-        textbox: {
-          ...Form.stylesheet.textbox,
-          normal: {
-            ...Form.stylesheet.textbox.normal,
-            height: 150,
-            width: screenWidth * 0.8,
-            textAlignVertical: 'top',
-          },
+    fields: {
+        body: {
+            placeholder: 'Your thoughts here...',
+            multiline: true,
+            numberOfLines: 5,
+            // blurOnSubmit: true,
+            maxLength: 1000,
+            stylesheet: {
+                ...Form.stylesheet,
+                textbox: {
+                    ...Form.stylesheet.textbox,
+                    normal: {
+                        ...Form.stylesheet.textbox.normal,
+                        height: 150,
+                        width: screenWidth * 0.8,
+                        textAlignVertical: 'top'
+                    }
+                }
+            }
         },
-      },
-    },
-    title: {
-      placeholder: 'Your clever title here...',
-      maxLength: 150,
-      stylesheet: {
-        ...Form.stylesheet,
-        textbox: {
-          ...Form.stylesheet.textbox,
-          normal: {
-            ...Form.stylesheet.textbox.normal,
-            height: null,
-            width: screenWidth * 0.8,
-          },
-        },
-      },
-    },
-  },
-};
+        title: {
+            placeholder: 'Your clever title here...',
+            maxLength: 150,
+            stylesheet: {
+                ...Form.stylesheet,
+                textbox: {
+                    ...Form.stylesheet.textbox,
+                    normal: {
+                        ...Form.stylesheet.textbox.normal,
+                        height: null,
+                        width: screenWidth * 0.8
+                    }
+                }
+            }
+        }
+    }
+}
 
 // container component for new post form
 export const NewPost = inject('store')(
-  class NewPost extends Component {
-    // validate submission, send submission, close parent modal
-    submitPost = async () => {
-      let results = this.form.validate();
-      let errors = results.errors;
-      // check if submission is valid -- there must be a title!
-      if (errors.length === 0) {
-        let { title, body } = results.value;
-        this.props.store.addPost(title, body); // store new post in state
-        this.form.setState({ value: null }); // clear form
+    class NewPost extends Component {
 
-        this.props.closeView(); // disable parent modal by changing its state
-      }
+        // validate submission, send submission, close parent modal
+        submitPost = async () => {
 
-      this.props.closeView(); // disable parent modal by changing its state
-    };
+            let results = this.form.validate()
+            let errors = results.errors
 
-    render() {
-      return (
-        <View style={styles.content}>
-          {/* new post creation form */}
-          <Form type={PostSchema} options={PostOptions} ref={c => (this.form = c)} />
-          <View>
-            <Button bordered info rounded onPress={() => this.submitPost()}>
-              <Text>Create New Post!</Text>
-            </Button>
-          </View>
-        </View>
-      );
+            // check if submission is valid -- there must be a title!
+            if (errors.length === 0) {
+                let {title, body} = results.value
+                this.props.store.addPost(title, body) // store new post in state
+
+                this.form.setState({value: null}) // clear form
+
+                this.props.closeView() // disable parent modal by changing its state
+            }
+
+        }
+
+        render() {
+            return (
+                <View style={styles.content}>
+                    {/* new post creation form */}
+                    <Form
+                        type={PostSchema}
+                        options={PostOptions}
+                        ref={c => this.form = c}
+                    />
+                    <View>
+                        <Button
+                            bordered
+                            info
+                            rounded
+                            onPress={() => this.submitPost()}
+                        >
+                            <Text>
+                                Create New Post!
+                            </Text>
+                        </Button>
+                    </View>
+                </View>
+            )
+        }
+    })
+
+
+const styles = StyleSheet.create(
+    {
+        content: {
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "white",
+        }
     }
-  }
-);
-
-const styles = StyleSheet.create({
-  content: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'white',
-  },
-});
+)
--- Screens/PostDetail.js
+++ Screens/PostDetail.js
@@ -1,176 +1,188 @@
-import React, { Component } from 'react';
-import { Header } from 'react-navigation';
-import { FlatList, Keyboard, KeyboardAvoidingView, RefreshControl, StyleSheet } from 'react-native';
-import { Button, Card, Icon, Input, View } from 'native-base';
-import { inject, observer } from 'mobx-react';
-import Blank from '../Components/Blank';
-import PostData from '../Components/PostData';
+import React, {Component} from 'react'
+import {Header} from 'react-navigation'
+import {FlatList, Keyboard, KeyboardAvoidingView, RefreshControl, StyleSheet} from 'react-native'
+import {Button, Card, Icon, Input, View} from 'native-base'
+import Blank from '../Components/Blank'
+import PostData from '../Components/PostData'
+import {inject, observer} from 'mobx-react';
 import CommentData from '../Components/CommentData';
 
 const PostDetailFooter = observer(
-  class PostDetailFooter extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        input: '',
-      };
-    }
+    class PostDetailFooter extends Component {
 
-    onSubmit() {
-      if (this.state.input) {
-        this.props.post.addComment(this.state.input);
-        this.setState({ input: '' });
-      }
-    }
+        constructor(props) {
+            super(props);
+            this.state = {
+                input: ''
+            };
+        }
 
-    render() {
-      let post = this.post;
-      return (
-        <KeyboardAvoidingView
-          keyboardVerticalOffset={Header.HEIGHT}
-          behavior="position"
-          keyboardShouldPersistTaps>
-          <View style={styles.commentInputContainer}>
-            {/* takes user comment input */}
-            <Input
-              placeholder="Your comment here..."
-              onChangeText={text => {
-                this.setState({ input: text });
-              }}
-              onSubmitEditing={() => {
-                this.onSubmit();
-              }}
-              value={this.state.input}
-            />
+        onSubmit() {
+            if (this.state.input) {
+                this.props.post.addComment(this.state.input)
+                this.setState({input: ''})
+            }
+        }
 
-            {/* submits comment*/}
-            <Button transparent>
-              <Icon
-                name="telegram"
-                type="MaterialCommunityIcons"
-                style={{ color: 'powderblue' }}
-                onPress={() => {
-                  this.onSubmit();
-                }}
-              />
-            </Button>
-          </View>
-        </KeyboardAvoidingView>
-      );
-    }
-  }
-);
+        render() {
+            let post = this.post
+            return (
+                <KeyboardAvoidingView
+                    keyboardVerticalOffset={Header.HEIGHT}
+                    behavior="position"
+                    keyboardShouldPersistTaps={false}>
+
+                    <View style={styles.commentInputContainer}>
+
+                        {/* takes user comment input */}
+                        <Input
+                            placeholder='Your comment here...'
+                            onChangeText={(text) => {
+                                this.setState({input: text})
+                            }}
+                            multiline={true}
+                            // onSubmitEditing={() => {
+                            //     // this.onSubmit()
+                            // }}
+                            value={this.state.input}
+                        />
+
+                        {/* submits comment*/}
+                        <Button transparent>
+                            <Icon name='telegram'
+                                  type='MaterialCommunityIcons'
+                                  style={{color: 'powderblue'}}
+                                  onPress={() => {
+                                      this.onSubmit()
+                                  }}
+                            />
+                        </Button>
+                    </View>
+                </KeyboardAvoidingView>
+            )
+        }
+    })
 
 // Comments container of custom comment components
 const Comments = observer(
-  class Comments extends Component {
-    render() {
-      let comments = this.props.comments;
+    class Comments extends Component {
 
-      return (
-        <FlatList
-          data={comments}
-          listKey={item => item._id}
-          keyExtractor={item => item._id}
-          renderItem={item => {
-            let comment = item.item;
+
+        render() {
+            let comments = this.props.comments
 
             return (
-              <Card>
-                <CommentData comment={comment} post_id={this.props.post_id} showVote />
-              </Card>
-            );
-          }}
-        />
-      );
-    }
-  }
-);
+                <FlatList
+                    data={comments}
+                    keyExtractor={(item, index) => item._id}
+                    renderItem={(item) => {
+                        let comment = item.item;
 
-let PostDetail;
-export default (PostDetail = inject('store')(
-  observer(
-    class PostDetailScreen extends Component {
-      // initialize with default values -- DO NOT fetch data here
-      constructor(props) {
-        super(props);
-        this.offset = 0;
+                        return (
+                            <Card>
+                                <CommentData
+                                    comment={comment}
+                                    post_id={this.props.post_id}
+                                    showVote={true}
+                                />
+                            </Card>
+                        )
+                    }}
+                />
+            )
+        }
+    })
 
-        this.state = {
-          refresh: false,
-        };
-      }
+export default PostDetail = inject('store')(
+    observer(
+        class PostDetailScreen extends Component {
 
-      _onRefresh = async () => {
-        this.setState(state => ({ refresh: true })); // indicate we are refreshing
-        let id = this.props.navigation.getParam('id');
-        let post = this.props.store.getPost(id);
+            // initialize with default values -- DO NOT fetch data here
+            constructor(props) {
+                super(props)
+                this.offset = 0
 
-        post.update().then(() => this.setState(state => ({ refresh: false }))); // refresh data
-      };
-
-      _renderItem = item => {
-        let post = item.item;
-
-        return (
-          <View style={{ flex: 1 }}>
-            <Card>
-              <PostData post={post} />
-            </Card>
-            <Comments comments={post.comments} post_id={post._id} />
-          </View>
-        );
-      };
-
-      _handleScroll = newY => {
-        return newY - this.offset <= 0;
-      };
-
-      // render a post with comments -- use posts component from main as an example for structure
-      render() {
-        let id = this.props.navigation.getParam('id');
-        let post = this.props.store.getPost(id);
-
-        // display posts in a list component
-        let refresh = this.state.refresh;
-
-        return (
-          <View style={{ flex: 1, backgroundColor: 'powderblue' }}>
-            <View style={{ flex: 1 }}>
-              {/*Scrolling list of comments + post*/}
-              <FlatList
-                data={[post]}
-                renderItem={item => {
-                  return this._renderItem(item);
-                }}
-                keyExtractor={(item, index) => item._id}
-                onScrollBeginDrag={e => (this.offset = e.nativeEvent.contentOffset.y)}
-                onScrollEndDrag={e =>
-                  this._handleScroll(e.nativeEvent.contentOffset.y) ? Keyboard.dismiss() : {}
+                this.state = {
+                    refresh: false
                 }
-                refreshControl={
-                  // controls refreshing
-                  <RefreshControl
-                    refreshing={refresh}
-                    onRefresh={this._onRefresh}
-                    tintColor="skyblue"
-                  />
-                }
-                ListEmptyComponent={<Blank />}
-                contentContainerStyle={
-                  post === undefined ? { flex: 1, alignItems: 'center', flexWrap: 'wrap' } : {}
-                }
-              />
-            </View>
+            }
 
-            {/* comments field */}
-            <PostDetailFooter post={post} />
-          </View>
-        );
-      }
-    }
-  )
-));
+            _onRefresh = async () => {
+                this.setState((state) => ({refresh: true})) // indicate we are refreshing
+                id = this.props.navigation.getParam('id')
+                post = this.props.store.getPost(id)
+
+                post.update()
+                    .then(() => this.setState((state) => ({refresh: false}))) // refresh data
+            }
+
+            _renderItem = (item) => {
+                let post = item.item
+
+                return (
+                    <View style={{flex: 1}}>
+                        <Card>
+                            <PostData
+                                post={post}
+                            />
+                        </Card>
+                        <Comments
+                            comments={post.comments}
+                            post_id={post._id}
+                        />
+                    </View>
+                )
+            }
+
+            _handleScroll = newY => {
+                isUp = newY - this.offset <= 0
+                return isUp
+            }
+
+            // render a post with comments -- use posts component from main as an example for structure
+            render() {
+                id = this.props.navigation.getParam('id')
+                post = this.props.store.getPost(id)
+
+                // display posts in a list component
+                let refresh = this.state.refresh
+
+                return (
+                    <View style={{flex: 1, backgroundColor: 'powderblue'}}>
+                        <View style={{flex: 1}}>
+
+                            {/*Scrolling list of comments + post*/}
+                            <FlatList
+                                data={[post]}
+                                renderItem={(item) => {
+                                    return this._renderItem(item)
+                                }}
+                                keyExtractor={(item, index) => item._id}
+                                onScrollBeginDrag={(e) => this.offset = e.nativeEvent.contentOffset.y}
+                                onScrollEndDrag={(e) => this._handleScroll(e.nativeEvent.contentOffset.y) ? Keyboard.dismiss() : {}}
+                                refreshControl={ // controls refreshing
+                                    <RefreshControl
+                                        refreshing={refresh}
+                                        onRefresh={this._onRefresh}
+                                        tintColor='skyblue'
+                                    />
+                                }
+                                ListEmptyComponent={<Blank/>}
+                                contentContainerStyle={(post == undefined) ? {
+                                    flex: 1,
+                                    alignItems: 'center',
+                                    flexWrap: 'wrap'
+                                } : {}}
+                            />
+                        </View>
+
+                        {/* comments field */}
+                        <PostDetailFooter post={post}/>
+                    </View>
+                )
+            }
+        }))
+
 
 const styles = StyleSheet.create({
     newPostButton: {
