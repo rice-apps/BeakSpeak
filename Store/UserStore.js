313864f2ad7e9537bfd06293b577dcae9514b94e
nquazi <noushinquazi@yahoo.com>
2019-04-02 14:42:27 -0500

    smarter network handling

--- Store/UserStore.js
+++ Store/UserStore.js
@@ -1,11 +1,14 @@
 import { observable, action, decorate } from 'mobx';
+import {NetInfo} from 'react-native'
 
 class UserStore {
     token = ""
     //"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InVzZXIiOiJubnExIiwiYXR0cmlidXRlcyI6eyJlZHVQZXJzb25QcmltYXJ5QWZmaWxpYXRpb24iOiJzdHVkZW50In19LCJ1c2VySUQiOiI1YjVmOWE5YWRlNTdiNzQxZmZjM2U2MWUiLCJpYXQiOjE1MzI5OTIxNTR9.cr29eYKLTpaAuqcpk08XtrMt6FZj9S8Yvll3rzEMYus";
 
-    isConnected = false
-
+    isConnected = true
+    constructor() {
+        NetInfo.isConnected.fetch().then(conn => this.isConnected = conn)
+    }
     getToken() {
         return this.token;
     }
