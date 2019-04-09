c1a32e35c63d80d5304f3d8633dfa313f151b818
nquazi <noushinquazi@yahoo.com>
2019-04-09 00:24:38 -0500

    labels to sidebar

--- Components/Sidebar.js
+++ Components/Sidebar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Alert, View, StyleSheet } from 'react-native';
+import { Alert, View, StyleSheet, TouchableOpacity, Text} from 'react-native';
 import { Icon } from 'native-base';
 import { SecureStore, WebBrowser } from "expo";
 import { CONFIG } from "../config";
@@ -9,48 +9,67 @@ export default class Sidebar extends Component {
     this.props.navigation.navigate(route);
   };
 
+  logout = () => {
+    Alert.alert('Log Out', 'Are you sure you want to log out?', [
+      {
+        text: 'Yes',
+        onPress: () => {
+          SecureStore.deleteItemAsync('token');
+          let returnUrl = Expo.Linking.makeUrl();
+          WebBrowser.openAuthSessionAsync
+          (CONFIG.cas_logout_url, returnUrl).then(result =>                   this.navigate('Front')
+          )
+        }
+      },
+      {
+        text: 'Cancel',
+        style: 'cancel',
+      },
+    ]);
+  }
+  
   render() {
     return (
       // each icon represents a page accessible from the menu
       <View style={styles.container}>
-        <Icon
-          name="note-text"
-          fontSize={30}
-          type="MaterialCommunityIcons"
-          style={{ color: 'white' }}
-          onPress={() => this.navigate('Main')}
-        />
-        <Icon
-          name="information"
-          fontSize={30}
-          type="MaterialCommunityIcons"
-          style={{ color: 'white' }}
-          onPress={() => this.navigate('Info')}
-        />
-        <Icon
-          name="logout"
-          fontSize={30}
-          type="MaterialCommunityIcons"
-          style={{ color: 'white' }}
-          onPress={() => {
-            Alert.alert('Log Out', 'Are you sure you want to log out?', [
-              {
-                text: 'Yes',
-                onPress: () => {
-                  SecureStore.deleteItemAsync('token');
-                  let returnUrl = Expo.Linking.makeUrl();
-                  WebBrowser.openAuthSessionAsync
-                  (CONFIG.cas_logout_url, returnUrl).then(result =>                   this.navigate('Front')
-                  )
-                }
-              },
-              {
-                text: 'Cancel',
-                style: 'cancel',
-              },
-            ]);
-          }}
-        />
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <TouchableOpacity
+            onPress={() => this.navigate('Main')}
+          >
+            <Icon
+              name="note-text"
+              fontSize={30}
+              type="MaterialCommunityIcons"
+              style={{ color: 'white' }}
+            />
+          </TouchableOpacity>
+          <Text style={{color: "white", fontSize: 25}}> Feed </Text>
+        </View>
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <TouchableOpacity
+            onPress={() => this.navigate('Info')}
+          >
+            <Icon
+              name="information"
+              fontSize={30}
+              type="MaterialCommunityIcons"
+              style={{ color: 'white' }}
+            />
+          </TouchableOpacity>
+          <Text style={{color: "white", fontSize: 25}}> Info </Text>
+        </View>
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+        <TouchableOpacity>
+          <Icon
+            name="logout"
+            fontSize={30}
+            type="MaterialCommunityIcons"
+            style={{ color: 'white' }}
+            onPress={this.logout}
+          />
+        </TouchableOpacity>
+        <Text style={{color: "white", fontSize: 25}}> Logout </Text>
+        </View>
       </View>
     );
   }
@@ -60,8 +79,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'space-evenly',
-    alignItems: 'center',
+    alignItems: 'flex-start',
     backgroundColor: 'powderblue',
+    padding: 5
   },
   seeBorders: {
     borderWidth: 1,
